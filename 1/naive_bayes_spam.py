0608b4e2a00e0ce22f0530e9d4b95f1dd149a9a7
unknown <yg2519@columbia.edu>
2018-09-23 14:06:25 -0400

    Add comments

--- 1/naive_bayes_spam.py
+++ 1/naive_bayes_spam.py
@@ -3,24 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as st
 
+# Load files using pandas
 label_train = pd.read_csv('label_train.csv', header=None)
 label_test = pd.read_csv('label_test.csv', header=None)
 x_train = pd.read_csv('x_train.csv', header=None)
 x_test = pd.read_csv('x_test.csv', header=None)
 
+# Read data parameters
 feature_number = x_train.shape[1]
 num_train = x_train.shape[0]
 num_test = x_test.shape[0]
 
 
+# Define the classifier
 class NBClassifier:
     def __init__(self, dim, num_class, a=1, b=1, e=1, f=1):
+        # Initialize with the given parameters
         self.dim = dim
         self.c = num_class
         self.r = np.zeros([self.c, self.dim])
         self.a, self.b, self.e, self.f = a, b, e, f
         self.py = np.zeros(2)
 
+    # Train the model by calculating distributions
     def train(self, xs, ys):
         self.py[0] = 1 / (self.b + ys[ys[0] == 0].shape[0] + 1)
         self.py[1] = 1 / (self.b + ys[ys[0] == 1].shape[0] + 1)
@@ -29,12 +34,14 @@ class NBClassifier:
         x_ones = xs[ys[0] == 1]
         self.r[0], self.r[1] = self.cal_r(x_zeros), self.cal_r(x_ones)
 
+    # Calculate parameters based on distribution
     def cal_r(self, xs):
         result = np.zeros(self.dim)
         for i in range(self.dim):
             result[i] = xs[i].sum()
         return result
 
+    # Predict label by taking the sum of all features' log probability
     def predict(self, px):
         prb = np.full(self.c, 1.0).astype(np.float64)
         for i in range(self.c):
@@ -42,6 +49,7 @@ class NBClassifier:
                 prb[i] += np.log(st.nbinom.pmf(k=px[j], n=self.r[i][j], p=1-self.py[i]))
         return prb
 
+    # Calculate the parameters for plot for (c) and (d)
     def average_lambda(self, xs, ys, y):
         # Not sure here
         result = []
@@ -61,12 +69,13 @@ nbc.train(x_train, label_train)
 prob = np.zeros([num_test, 2], dtype=np.float64)
 bad_case = []
 for i, x in enumerate(x_test.values):
-    prob[i] = nbc.predict(x)
+    prob[i] = nbc.predict(x) # Predict test data
 
 
 pred = np.array([a < b for a, b in prob]).astype(int)
 
 
+# Calculate the table of prediction result
 def cal_result(pred_y, label):
     result = np.zeros([2, 2])
     for p, l in zip(pred_y, label.values):
@@ -77,6 +86,7 @@ def cal_result(pred_y, label):
 print(cal_result(pred, label_test))
 
 
+# Select mis-classified emails in test data
 def choose_mis(pred_y, label):
     misidx = []
     for i, p in enumerate(zip(pred_y, label.values)):
@@ -85,6 +95,7 @@ def choose_mis(pred_y, label):
     return misidx
 
 
+# Load feature name from readme for plot
 names = []
 with open('README.md', 'r') as f:
     for l in f:
@@ -112,9 +123,11 @@ for i, m in enumerate(idx):
     plt.savefig('mis_'+str(i))
     #plt.show()
 
-most_ambi = np.array([np.abs(p[0]-p[1]) for p in prob])
+# Calculate the real probability and pick the three most ambiguity emails.
+prob_c = [[np.divide(np.exp(a), np.add(np.exp(a), np.exp(b))), np.divide(np.exp(b), np.add(np.exp(a), np.exp(b)))] for a, b in prob]
+most_ambi = np.array([np.abs(p[0]-p[1]) for p in prob_c])
 most_ambi.sort()
-ambi_idx = [i for i, p in enumerate(prob) if np.abs(p[0]-p[1]) in set(most_ambi[:3])]
+ambi_idx = [i for i, p in enumerate(prob_c) if np.abs(p[0]-p[1]) in set(most_ambi[:3])]
 idx = ambi_idx
 print(x_test.iloc[idx], pred[idx], [prob[i] for i in idx], label_test.iloc[idx].values)
 for i, m in enumerate(idx):
@@ -132,6 +145,3 @@ for i, m in enumerate(idx):
     plt.savefig("Ambig_" + str(i))
     #plt.show()
 
-#accuray needs 461 88% non-83% spam higher 95%
-#mine: 86%, 81.4%, 94.5%
-#sklearn multiNB: 86%, 80%, 95.6%
