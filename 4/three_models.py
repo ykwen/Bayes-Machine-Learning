c031c57e6881801b668a2bd1c80b971c532e976a
ykwen <yg2519@columbia.edu>
2018-11-27 02:42:16 -0500

    fixed vi

--- 4/three_models.py
+++ 4/three_models.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import beta
-from scipy.special import comb, digamma
+from scipy.stats import beta, multinomial, dirichlet
+from scipy.special import comb, digamma, gamma, factorial
 from heapq import nlargest
 import pandas as pd
 
@@ -12,7 +12,7 @@ class EM:
 
         theta, pi = np.random.rand(K), np.random.rand(K)
         self.theta, self.pi = np.exp(theta) / np.sum(np.exp(theta)), np.exp(pi) / np.sum(np.exp(pi))
-        self.pi_tmp = np.zeros([self.N, K])
+        self.phi = np.zeros([self.N, K])
 
         self.likelihood = 0
 
@@ -33,13 +33,13 @@ class EM:
     def e_step(self):
         for i, xx in enumerate(self.x):
             tmp = comb(20, xx) * (self.theta ** xx) * ((1 - self.theta) ** (20 - xx)) * self.pi
-            self.pi_tmp[i] = tmp / sum(tmp)
+            self.phi[i] = tmp / sum(tmp)
 
     def m_step_pi(self):
-        self.pi = np.sum(self.pi_tmp, axis=0) / self.N
+        self.pi = np.sum(self.phi, axis=0) / self.N
 
     def m_step_theta(self):
-        self.theta = np.sum(self.pi_tmp * self.x[:, None], axis=0) / np.sum(self.pi_tmp * 20, axis=0)
+        self.theta = np.sum(self.phi * self.x[:, None], axis=0) / np.sum(self.phi * 20, axis=0)
 
     def cal_log_likelihood(self):
         self.likelihood = 0
@@ -54,63 +54,104 @@ class EM:
 class VI:
     def __init__(self, X, K, alpha0, a0, b0):
         self.X, self.N, self.K = X, len(X), K
-        self.alpha0, self.a0, self.b0 = np.float64(alpha0), np.float64(a0), np.float64(b0)
-        self.alpha_t, self.a_t, self.b_t = np.random.rand(K), np.repeat(self.a0, K), np.repeat(self.b0, K)
-        theta, pi = np.random.rand(K), np.random.rand(self.N * K).reshape([-1, K])
-        self.theta, self.pi = np.exp(theta) / np.sum(np.exp(theta)), np.exp(pi) / np.sum(np.exp(pi), axis=1)[:, None]
+        self.alpha0, self.a0, self.b0 = alpha0, np.float64(a0), np.float64(b0)
+        self.alpha_t, self.a_t, self.b_t = np.random.rand(K) + self.alpha0, np.repeat(self.a0, K), np.repeat(self.b0, K)
+        phi = np.random.rand(self.N * K).reshape([self.N, K])
+        self.phi = np.exp(phi) / np.sum(np.exp(phi), axis=1)[:, None]
 
     def train(self, num_iter):
         r = []
         for _ in range(num_iter):
-            tmp_pi_a = np.repeat([digamma(self.a_t) + digamma(self.b_t)], self.N, axis=0)
-            tmp_pi_b = np.repeat([digamma(self.a_t + self.b_t)], self.N, axis=0)
-            tmp_pi_c = np.repeat([digamma(self.alpha_t)], self.N, axis=0)
-            tmp_pi = np.exp(tmp_pi_a * self.X[:, None]
-                            + tmp_pi_b * (20 - 2 * self.X)[:, None] + tmp_pi_c)
-            self.pi = tmp_pi / np.sum(tmp_pi, axis=1)[:, None]
 
-            self.alpha_t = self.alpha0 + np.sum(self.pi, axis=0)
+            phi_a = np.repeat([digamma(self.a_t) - digamma(self.a_t + self.b_t)], self.N, axis=0)
+            phi_b = np.repeat([digamma(self.b_t) - digamma(self.a_t + self.b_t)], self.N, axis=0)
+            phi_alpha = np.repeat([digamma(self.alpha_t) - digamma(np.sum(self.alpha_t))], self.N, axis=0)
+            tmp_phi = np.exp(phi_a * self.X[:, None]
+                             + phi_b * (20 - self.X)[:, None] + phi_alpha)
+            self.phi = tmp_phi / np.sum(tmp_phi, axis=1)[:, None]
 
-            self.a_t = self.a0 + np.sum(self.pi * self.X[:, None], axis=0)
-            self.b_t = self.b0 + np.sum(self.pi * (20 - self.X)[:, None], axis=0)
+            self.alpha_t = self.alpha0 + np.sum(self.phi, axis=0)
+
+            self.a_t = self.a0 + np.sum(self.phi * self.X[:, None], axis=0)
+            self.b_t = self.b0 + np.sum(self.phi * (20 - self.X)[:, None], axis=0)
 
             r.append(self.cal_l())
         return r
 
     def cal_l(self):
-        c = np.argmax(self.pi, axis=1)
+        c = np.argmax(self.phi, axis=1)
         a = np.array([self.a_t[c_i] for c_i in c])
         b = np.array([self.b_t[c_i] for c_i in c])
         alpha = np.array([self.alpha_t[c_i] for c_i in c])
 
-        tmp_pi_a, tmp_pi_b = digamma(a) + digamma(b), digamma(a + b)
-        p_X = np.sum(tmp_pi_a * self.X + tmp_pi_b * (20 - 2 * self.X))
-        p_c = np.sum(digamma(alpha)) - self.N * digamma(np.sum(self.alpha_t))
-        p_theta = np.sum((self.a0 - 1) * digamma(self.a_t) -
-                         (self.b0 - 1) * digamma(self.b_t) +
-                         (self.b0 - self.a0) * digamma(self.a_t + self.b_t))
-        p_pi = np.sum((self.alpha0 - 1) * (digamma(self.alpha_t) - digamma(np.sum(self.alpha_t))))
-        q_pi = np.sum(digamma(self.alpha_t) - digamma(np.sum(self.alpha_t)))
-        q_theta = np.sum(digamma(self.a_t) - digamma(self.a_t + self.b_t))
-        q_c = np.sum(self.pi * np.log(self.pi))
-        return p_X + p_c + p_theta + p_pi - q_pi - q_theta - q_c
+        # avoid overflow or divide 0 in digamma functions
+        stand = np.float64(0.0000001)
+        a[a < stand], b[b < stand], c[c < stand] = stand, stand, stand
+        a_t, b_t, alpha_t = self.a_t.copy(), self.b_t.copy(), self.alpha_t.copy()
+        a_t[a_t < stand], b_t[b_t < stand], alpha_t[alpha_t < stand] = stand, stand, stand
+
+        phi_a = digamma(a_t) - digamma(a_t + b_t)
+        phi_b = digamma(b_t) - digamma(a_t + b_t)
+        phi_a_x = digamma(a) - digamma(a + b)
+        phi_b_x = digamma(b) - digamma(a + b)
+        phi_alpha, phi_alpha0 = digamma(alpha), digamma(np.sum(alpha_t))
+        p_x = np.sum(phi_a_x * self.X + phi_b_x * (20 - self.X))
+        p_c = np.sum(digamma(alpha)) - digamma(np.sum(self.alpha_t))
+        p_theta = np.sum((self.a0 - 1) * phi_a + (self.b0 - 1) * phi_b)
+        p_pi = np.sum((self.alpha0 - 1) * (phi_alpha - phi_alpha0))
+
+        q_pi = np.sum(dirichlet.entropy(self.alpha_t))
+        q_theta = np.sum(beta.entropy(self.a_t, self.b_t))
+        q_c = np.sum(
+            -20 * np.log(factorial(20)) - \
+            20 * np.sum(self.phi * np.log(self.phi), axis=1) + \
+            self.multi()
+        )
+
+        '''
+        q_pi = np.sum(
+            np.log(np.prod(gamma(alpha_t)) / gamma(np.sum(alpha_t))) + \
+            (np.sum(alpha_t) - self.K) * digamma(np.sum(alpha_t)) - \
+            np.sum((alpha_t - 1) * digamma(alpha_t))
+        )
+        q_theta = np.sum(
+            np.log(gamma(a_t) * gamma(b_t) / gamma(a_t + b_t)) - \
+            (a_t - 1) * digamma(a_t) - (b_t - 1) * digamma(b_t) + \
+            (a_t + b_t - 2) * digamma(a_t + b_t)
+        )
+        q_c = np.sum(multinomial.entropy(20, self.phi))
+        '''
+        if any(np.isnan(np.array([p_x, p_c, p_theta, p_pi, q_pi, q_theta, q_c]))):
+            print(self.K)
+            print(p_x, p_c, p_theta, p_pi, q_pi, q_theta, q_c)
+        return p_x + p_c + p_theta + p_pi - q_pi - q_theta - q_c
+
+    def multi(self):
+        r = np.zeros([self.N, self.K])
+        comb_result = comb(np.repeat(20, 21), np.arange(21))
+        log_j = np.log(factorial(np.arange(21)))
+        for i in range(21):
+            r += comb_result[i] * log_j[i] * (self.phi ** i) * ((1 - self.phi) ** (20 - i))
+        return np.sum(r, axis=1)
 
     def predict(self, xs):
         n = len(xs)
-        tmp_pi_a = np.repeat([digamma(self.a_t) + digamma(self.b_t)], n, axis=0)
-        tmp_pi_b = np.repeat([digamma(self.a_t + self.b_t)], n, axis=0)
-        tmp_pi_c = np.repeat([digamma(self.alpha_t)], n, axis=0)
-        tmp_pi = tmp_pi_a * xs[:, None] + tmp_pi_b * (20 - 2 * xs)[:, None] + tmp_pi_c
-        return np.argmax(tmp_pi, axis=1)
+        phi_a = np.repeat([digamma(self.a_t) - digamma(self.a_t + self.b_t)], n, axis=0)
+        phi_b = np.repeat([digamma(self.b_t) - digamma(self.a_t + self.b_t)], n, axis=0)
+        phi_alpha = np.repeat([digamma(self.alpha_t) - digamma(np.sum(self.alpha_t))], n, axis=0)
+        tmp_phi = np.exp(phi_a * xs[:, None]
+                         + phi_b * (20 - xs)[:, None] + phi_alpha)
+        return np.argmax(tmp_phi, axis=1)
 
 
 class Gibbs:
-    def __init__(self, X, K, alpha, a, b):
-        self.X, self.alpha0, self.a0, self.b0 = X, alpha, a, b
-        self.N, self.K = len(self.X), K
-        self.a_t, self.b_t = np.repeat(self.a0, K), np.repeat(self.b0, K)
+    def __init__(self, X, K, alpha0, a0, b0):
+        self.X, self.N, self.K = X, len(X), K
+        self.alpha0, self.a0, self.b0 = alpha0, np.float64(a0), np.float64(b0)
+        self.alpha_t, self.a_t, self.b_t = np.random.rand(K) + self.alpha0, np.repeat(self.a0, K), np.repeat(self.b0, K)
+
         self.c = np.zeros(self.N).astype(np.float64)
-        theta, pi = np.random.rand(K), np.random.rand(self.N * K).reshape([-1, K])
+        theta, pi = np.random.rand(K), np.random.rand(self.N * K).reshape([self.N, K])
         self.theta, self.pi = np.exp(theta) / np.sum(np.exp(theta)), np.exp(pi) / np.sum(np.exp(pi), axis=1)[:, None]
 
     def train(self, num_iters):
@@ -204,5 +245,6 @@ if __name__ == '__main__':
     plt.plot(np.arange(t), num)
     plt.savefig("3_c")
     plt.show()
+
 
 
